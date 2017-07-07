b080d6ad05612ebe5431902cce9848ce6997c112
Stuefer Manuel <mstuefer@gmail.com>
2017-07-07 21:14:36 +0200

    Add list of filenames in which dup lines appear

--- ch1/06-dup.go
+++ ch1/06-dup.go
@@ -7,21 +7,33 @@ import (
 	"strings"
 )
 
+// prints: count, text and list of filenames, of lines that appear more than once
+
 func main() {
 	counts := make(map[string]int)
+	files := make(map[string]map[string]bool)
 	for _, filename := range os.Args[1:] {
 		data, err := ioutil.ReadFile(filename)
+		// NB, ReadFile returns a byte slice and must therefore converted into a string later. see string(data) in strings.Split()
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "dup3: %v\n", err)
 			continue
 		}
 		for _, line := range strings.Split(string(data), "\n") {
 			counts[line]++
+			if files[line] == nil {
+				files[line] = make(map[string]bool)
+			}
+			files[line][filename] = true
 		}
 	}
 	for line, n := range counts {
 		if n > 1 {
-			fmt.Printf("%d\t%s\n", n, line)
+			fmt.Printf("%d\t%s\t\t", n, line)
+			for file, _ := range files[line] {
+				fmt.Printf("%s ", file)
+			}
+			fmt.Println()
 		}
 	}
 }
