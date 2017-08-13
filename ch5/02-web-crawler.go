6e04a9ab43f1791931f735580dcb2a282138721d
Stuefer Manuel <mstuefer@gmail.com>
2017-08-13 18:28:22 +0200

    Add ch5/02-web-crawler.go

---
+++ ch5/02-web-crawler.go
@@ -0,0 +1,38 @@
+// 02-web-crawler https://www.google.com/
+// crawles the web in a breadthFirst manner, printing all links found
+
+package main
+
+import (
+	"./links"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	breadthFirst(crawl, os.Args[1:])
+}
+
+func breadthFirst(f func(item string) []string, worklist []string) {
+	seen := make(map[string]bool)
+	for len(worklist) > 0 {
+		items := worklist
+		worklist = nil
+		for _, item := range items {
+			if !seen[item] {
+				seen[item] = true
+				worklist = append(worklist, f(item)...) // f(item)... causes all items returned by f to be appended on worklist
+			}
+		}
+	}
+}
+
+func crawl(url string) []string {
+	fmt.Println(url)
+	list, err := links.Extract(url)
+	if err != nil {
+		log.Print(err)
+	}
+	return list
+}
