fc4cfd8366888c1c35ecadeab44fbf5650809b1a
Stuefer Manuel <mstuefer@gmail.com>
2017-08-14 14:59:39 +0200

    Add method with pointer receiver ScaleBy to 00-geometry

--- ch6/00-geometry.go
+++ ch6/00-geometry.go
@@ -16,10 +16,24 @@ func (p Point) EuclideanDistance(q Point) float64 {
 	return math.Hypot(q.X-p.X, q.Y-p.Y)
 }
 
+// usually calling a function make a copy of each argument value, whenever
+// we need to update a variable, or an argument is too large and we therefore
+// whish to avoid copying, we pass the address of the variable using a pointer
+// the same goes for methods that need to update the receiver variable
+func (p *Point) ScaleBy(factor float64) {
+	p.X *= factor
+	p.Y *= factor
+}
+
 func main() {
 	fmt.Println("Point p(1,2)")
 	p := Point{1, 2}
 	fmt.Println("Point q(4,6)")
 	q := Point{4, 6}
 	fmt.Println("Distance: ", p.EuclideanDistance(q))
+
+	fmt.Println("Scale point p by 2")
+	p.ScaleBy(2) // should be (&p).ScaleBy .. however, the compiler helps us out, translating p -> (&p)
+	// If the receiver p is a variable of type Point but the method requires *Point receiver, compiler will perform implicit &p on the variable
+	fmt.Println("p.X:", p.X, " p.Y:", p.Y)
 }
